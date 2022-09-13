5370ecd054e31c74ad248abb23166a6a0b3362d0
naorkiani <94603740+naorkiani@users.noreply.github.com>
2022-09-13 13:03:39 +0300

    Create contact.js

---
+++ server/routes/userSystem/users/contact.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const { Contact } = require("../../../model/ContactModel");
+const validCon = require("../../../validation/contact-usValid");
+router.post("/", async (req, res) => {
+  try {
+    const getContact = await validCon.contactSchema.validateAsync(req.body, {
+      abortEarly: false,
+    });
+    console.log(getContact);
+    const newContact = new Contact({
+      name: getContact.name,
+      email: getContact.email,
+      subject: getContact.subject,
+      message: getContact.message,
+    });
+    await newContact.save();
+    res.json({ status: 200, msg: "ok, succeeded", response: getContact });
+  } catch (error) {
+    res.send(error).status(400);
+  }
+});
+
+module.exports = router;
