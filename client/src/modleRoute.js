47a1548838d825f882e678a33e81b22d4b404b1d
naorkiani <94603740+naorkiani@users.noreply.github.com>
2022-09-13 13:06:56 +0300

    model

---
+++ client/src/modleRoute.js
@@ -0,0 +1,12 @@
+const ROUTES = {
+  ROOT: { to: "/", label: "Home" },
+  ABOUT: { to: "/about", label: "About" },
+
+  LOGOUT: { to: "/logout", label: "Logout" },
+  MY_CARDS: { to: "/fly-card", label: "Fly-Card" },
+  MY_FAV_CARDS: { to: "/my-fly-cards", label: "My Fly Cards" },
+  LOGIN: { to: "/login", label: "Login" },
+  SIGNUP: { to: "/signup", label: "Signup" },
+};
+
+export { ROUTES };
