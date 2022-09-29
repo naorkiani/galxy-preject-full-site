56a16b0b8042aeb6a0b0a9bf73f718f2e21f65f4
naorkiani <94603740+naorkiani@users.noreply.github.com>
2022-09-29 13:11:43 +0300

    Update userSystem.js

--- server/routes/userSystem/userSystem.js
+++ server/routes/userSystem/userSystem.js
@@ -6,9 +6,11 @@ const middleWareRouter = require("../../meddelware/authMiddlewere");
 const registerRouter = require("./users/register");
 const loginRouter = require("../userSystem/users/login");
 const findRouter = require("../userSystem/users/find");
+const contact = require("../userSystem/users/contact");
 
 router.use("/register", registerRouter);
 router.use("/login", loginRouter);
 router.use("/find", middleWareRouter, findRouter);
+router.use("/contact", contact);
 
 module.exports = router;
